1dd15c6deb7ff5c2ae3a79dfaf5398e54c542d40
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-25 18:58:44 -0400

    feat: database scripts and set env config

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,28 +1,13 @@
 import '../setup';
-import pg, { PoolConfig } from 'pg';
+import pg from 'pg';
 
 const { Pool } = pg;
-interface SSLConfig extends PoolConfig {
-  connectionString?: string;
-}
 
-let config: SSLConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: Number(process.env.DB_PORT),
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-};
-
-if (process.env.NODE_ENV === 'prod') {
-  config = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-}
-
-const connection = new Pool(config);
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default connection;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@
 import './setup';
 import app from './app';
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT} at ${process.env.NODE_ENV} mode`);
+app.listen(4000, () => {
+  console.log(`Server is listening on port $4000 at ${process.env.NODE_ENV} mode`);
 });
